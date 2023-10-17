795011bcab409e907fca3120a3294dc7fd145ccd
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-10-17 14:54:52 +1000

    Test for calculator, caesarCipher, analyzeArray

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-export { capitalize, reverseString };
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
 
 function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1);
@@ -10,4 +10,87 @@ function reverseString(string) {
     reverse += string[i];
   }
   return reverse;
+}
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
+
+function caesarCipher(string, shift) {
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  string = string.toLowerCase();
+
+  let encryptedString = "";
+
+  for (let i = 0; i < string.length; i++) {
+    let cipherIndex = alphabet.indexOf(`${string[i]}`) + shift;
+
+    if (cipherIndex > 25) {
+      cipherIndex -= 26;
+    }
+
+    if (!alphabet.includes(string[i])) {
+      encryptedString += string[i];
+    } else {
+      encryptedString += alphabet[cipherIndex];
+    }
+  }
+
+  return encryptedString;
+}
+
+function analyzeArray(array) {
+  let average =
+    array.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0) / array.length;
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+  let length = array.length;
+
+  return {
+    average,
+    min,
+    max,
+    length,
+  };
 }
--- practice.test.js
+++ practice.test.js
@@ -1,17 +1,62 @@
-import { capitalize, reverseString } from "./practice";
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./practice";
 
-test("first character only is not capitalized", () => {
+test("first character only is capitalized", () => {
   expect(capitalize("string")).toBe("String");
 });
 
-test("string is not reversed", () => {
+test("string 'string' is reversed", () => {
   expect(reverseString("string")).toBe("gnirts");
 });
 
-test("string is not reversed", () => {
-  expect(reverseString("elephant")).toBe("tnahpele");
+test("string 'a' is reversed", () => {
+  expect(reverseString("a")).toBe("a");
 });
 
-test("string is not reversed", () => {
-  expect(reverseString("a")).toBe("a");
+test("calculator add 1 + 2 is 3", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("calculator subtract 3 - 1 is 2", () => {
+  expect(calculator.subtract(3, 1)).toBe(2);
+});
+
+test("calculator divide 6 / 2 is 3", () => {
+  expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test("calculator multiply 4 * 3 is 12", () => {
+  expect(calculator.multiply(4, 3)).toBe(12);
+});
+
+test("caesar cipher", () => {
+  expect(caesarCipher("attack", 1)).toBe("buubdl");
+});
+
+test("caesar cipher shift 3", () => {
+  expect(caesarCipher("attack", 3)).toBe("dwwdfn");
+});
+
+test("caesar cipher with spaces", () => {
+  expect(caesarCipher("JavaScript Example of the Caesar Cipher ", 1)).toBe(
+    "kbwbtdsjqu fybnqmf pg uif dbftbs djqifs "
+  );
+});
+
+test("caesar cipher shift at end", () => {
+  expect(caesarCipher("xyz", 4)).toBe("bcd");
+});
+
+test("object average, min, max, length satisfied", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
